2514f4a7221ccee9592b09fe1e723d84b2d33855
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-07-07 15:56:20 +0200

    menu for restaurant

---
+++ restaurant/school/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into school_address(city,zip_code,street,local_number)values ('Kraśnik','23-23','Niepodległości',54);
+insert into school (school_name,uuid,email,address_id,restaurant_id) values ('Spoleczna','6a011133-1ca9-43fe-ac59-7d17b1b7a84d','lukasz.bak@interiowy.pl',1,0);
