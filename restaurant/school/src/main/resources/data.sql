3eaf197b761ffc36ec9f0b79aec97708cf0c8eec
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-07-07 19:23:30 +0200

    data.sql

--- restaurant/school/src/main/resources/data.sql
+++ restaurant/school/src/main/resources/data.sql
@@ -1,2 +1,11 @@
 insert into school_address(city,zip_code,street,local_number)values ('Kraśnik','23-23','Niepodległości',54);
-insert into school (school_name,uuid,email,address_id,restaurant_id) values ('Spoleczna','6a011133-1ca9-43fe-ac59-7d17b1b7a84d','lukasz.bak@interiowy.pl',1,0);
+insert into school (school_name,uuid,email,address_id,restaurant_id) values ('Spoleczna','6a011133-1ca9-43fe-ac59-7d17b1b7a84d','lukasz.bak@interiowy.pl',1,1);
+
+insert into school_address(city,zip_code,street,local_number)values ('Lublin','23-23','a.Kraśnicka',2);
+insert into school (school_name,uuid,email,address_id,restaurant_id) values ('liceum','22856f82-a47a-4c8c-a5e0-564b1608f1d6','.1234@interiowy.pl',2,2);
+
+insert into school_address(city,zip_code,street,local_number)values ('Urzędów','23-250','wodna',12);
+insert into school (school_name,uuid,email,address_id,restaurant_id) values ('Władysława Jagiello','48b71104-fa41-45a6-9f36-a58888f11232','acb@interiowy.pl',3,3);
+
+insert into school_address(city,zip_code,street,local_number)values ('Błażowa','36-030','3-go Maja',4);
+insert into school (school_name,uuid,email,address_id,restaurant_id) values ('Podstawowa','e9325c79-0e2f-421a-b274-514c67469927','0998@interiowy.pl',4,4);
